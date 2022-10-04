6a73fa035d5ddc27a0c60bae34ab99484f31d2d9
Samarth H Chinivar <samarthharish321@gmail.com>
2022-10-04 17:34:15 +0530

    day-1 assignment

---
+++ day-1-patterns/day-1-assignments/d1a5.java
@@ -0,0 +1,24 @@
+public class d1a5 {
+    public static void main(String[] args)
+    {
+        for(int i=1; i<=5; i++)
+        {
+            for(int j=1; j<=5; j++)
+            {
+                if(j<=5-i)
+                    System.out.print("  ");
+                else
+                    System.out.print("* ");
+            }
+            System.out.println();
+        }
+    }
+}
+
+/*  O/P:
+	*
+   **
+  ***
+ ****
+*****
+ */
