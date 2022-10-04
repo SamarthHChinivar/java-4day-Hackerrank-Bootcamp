7a5a25d28f23fdfcfc3893bad6ad4dfdd382ab82
Samarth H Chinivar <samarthharish321@gmail.com>
2022-10-04 17:41:24 +0530

    day-1 assignment

---
+++ day-1-patterns/day-1-assignments/d1a10.java
@@ -0,0 +1,21 @@
+public class d1a10 {
+    public static void main(String[] args)
+    {
+        for(int i=1; i<=5; i++)
+        {
+            for(int j=i; j<=5; j++)
+            {
+               System.out.print(j+ " "); 
+            }
+            System.out.println();
+        }
+    }
+}
+
+/*  O/P:
+1 2 3 4 5
+2 3 4 5
+3 4 5
+4 5
+5
+ */
